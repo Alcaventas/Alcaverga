ef502b575d53b22414329284ff93f545db2468f3
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2024-08-15 08:01:20 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -1,12 +1,10 @@
-import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
 import { smsg } from './lib/simple.js'
 import { format } from 'util' 
 import { fileURLToPath } from 'url'
 import path, { join } from 'path'
 import { unwatchFile, watchFile } from 'fs'
 import chalk from 'chalk'
-import ws from 'ws';
-import './plugins/main-allfake.js'
+import fetch from 'node-fetch'
 
 const { proto } = (await import('@whiskeysockets/baileys')).default
 const isNumber = x => typeof x === 'number' && !isNaN(x)

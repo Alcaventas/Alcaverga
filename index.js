85d2a106da38fa91a06cbae44e41a35738552c41
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2024-08-15 00:20:29 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Yaemori\nBot', {
+say('Alcabot\nBot', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • OfcDiego`, {
+say(`Developed By • Alcashop`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
